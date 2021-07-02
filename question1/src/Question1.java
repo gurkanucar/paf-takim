342e31fddbbe0ca2af6e26c1d202a64ee5d55b35
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-07-02 17:46:40 +0300

    Question 1 solved

--- question1/src/Question1.java
+++ question1/src/Question1.java
@@ -1,8 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Question1 {
 
     public static void main(String[] args) {
+        Map<String, String> phoneBook = new HashMap<>();
 
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        in.nextLine();
+        for(int i=0;i<n;i++)
+        {
+            String name=in.nextLine();
+            int phone=in.nextInt();
+            in.nextLine();
+            phoneBook.put(name,phone+"");
+        }
+        while(in.hasNext())
+        {
+            String s=in.nextLine();
+            String result = phoneBook.getOrDefault(s,null);
+
+            if (result == null) {
+                System.out.println("Not found");
+            } else {
+                System.out.println(s + "=" + result);
+            }
+
+        }
     }
 
 }
